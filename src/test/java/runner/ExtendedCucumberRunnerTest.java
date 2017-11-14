fa812e53065df3163a6c5cbaaf2cbf25fc15280a
mrunalgosar <predator.terminator2@gmail.com>
2017-11-15 02:32:13 +0530

    Initial test commit

--- src/test/java/runner/ExtendedCucumberRunnerTest.java
+++ src/test/java/runner/ExtendedCucumberRunnerTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(ExtendedParallelCucumber.class)
-@ExtendedCucumberOptions(threadsCount = 2, outputFolder = "results/", jsonReport = "results/cucumber.json"
+@ExtendedCucumberOptions(threadsCount = 2, outputFolder = "results/", jsonReport = "results/cucumber.json", retryCount = 2
         , detailedReport = true, detailedAggregatedReport = true, overviewReport = true, overviewChartsReport = true, featureOverviewChart = true, coverageReport = true, consolidatedReport = true, consolidatedReportConfig = "src/test/resources/conf/consolidated_batch.json")
 @CucumberOptions(features = "src/test/resources/features", glue = "stepdefs", plugin = {"html:results/cucumber-html-report", "json:results/cucumber.json", "usage:results/cukes-usage.json"})
 public class ExtendedCucumberRunnerTest {
