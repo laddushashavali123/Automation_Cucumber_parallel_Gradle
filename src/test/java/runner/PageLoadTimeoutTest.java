8acd5b2006fb979613e9da44f28ef25b05d52e1a
mrunalgosar <predator.terminator2@gmail.com>
2017-12-27 11:19:02 +0530

    Added a test for the pageloadTimeout issue

---
+++ src/test/java/runner/PageLoadTimeoutTest.java
@@ -0,0 +1,22 @@
+package runner;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class PageLoadTimeoutTest {
+
+    @Test
+    public void test() throws MalformedURLException {
+        System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\browserBinaries\\geckodriver.exe");
+        WebDriver browser = new FirefoxDriver();
+        browser.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        browser.manage().window().maximize();
+        browser.get("http://www.snapdeal.com");
+        System.out.println("Wait");
+        browser.close();
+    }
+}
