4c644a51c8ca4a2ee0b5062789403326ad3ee8cd
mrunalgosar <predator.terminator2@gmail.com>
2018-01-13 01:43:00 +0530

    Reporting fix

--- src/test/java/runner/ExtendedCucumberRunnerTest.java
+++ src/test/java/runner/ExtendedCucumberRunnerTest.java
@@ -5,12 +5,14 @@ import com.github.mkolisnyk.cucumber.runner.ExtendedParallelCucumber;
 import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(ExtendedParallelCucumber.class)
-@ExtendedCucumberOptions(threadsCount = 3, outputFolder = "results/", jsonReport = "results/cucumber.json", retryCount = 0
+@ExtendedCucumberOptions(threadsCount = 3, outputFolder = "results/", jsonReport = "results/cucumber.json", retryCount = 1
         , detailedReport = true, detailedAggregatedReport = true, overviewReport = true, overviewChartsReport = true, featureOverviewChart = true, coverageReport = true,
         consolidatedReport = true, consolidatedReportConfig = "src/test/resources/conf/consolidated_batch.json", usageReport = true,
         jsonUsageReport = "results/cukes-usage.json")
 @CucumberOptions(features = "src/test/resources/features", glue = "stepdefs", plugin = {"html:results/cucumber-html-report", "json:results/cucumber.json",
         "usage:results/cukes-usage.json"})
 public class ExtendedCucumberRunnerTest {
+
 }
--- src/test/java/runner/JUnitRunner.java
+++ src/test/java/runner/JUnitRunner.java
@@ -2,14 +2,7 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by mrunal on 7/16/2017.
@@ -19,21 +12,4 @@ import java.util.List;
         plugin = {"html:cucumberResults/cucumber-html-report", "json:cucumberResults/cucumber.json", "util.CustomCucumberListner:ExtentReport.html"}, tags = {"~@Ignore"})
 public class JUnitRunner {
 
-    @AfterClass
-    public static void afterClass() {
-        File reportOutputDirectory = new File("cucumberResults");
-        List<String> jsonFiles = new ArrayList<>();
-        jsonFiles.add("cucumberResults/cucumber.json");
-        String buildNumber = "1";
-        String projectName = "cucumberProject";
-        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
-        configuration.setParallelTesting(false);
-        configuration.setRunWithJenkins(true);
-        configuration.setBuildNumber(buildNumber);
-        configuration.addClassifications("Platform", "Windows");
-        configuration.addClassifications("Browser", "Chrome");
-        configuration.addClassifications("Branch", "release/1.0");
-        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
-        reportBuilder.generateReports();
-    }
 }
